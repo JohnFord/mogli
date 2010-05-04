a7ea556478c6af4348d99f853ee805100056e144
John Ford <vitrueuser@John-Fords-MBP.local>
2010-05-04 10:43:23 -0400

    Added privacy property to Post

--- lib/mogli/post.rb
+++ lib/mogli/post.rb
@@ -4,7 +4,7 @@ module Mogli
     include Model
     define_properties :id, :to, :message, :picture, :link, :name, :caption, 
       :description, :source, :icon, :attribution, :actions, :likes,
-      :created_time, :updated_time
+      :created_time, :updated_time, :privacy
     
     hash_populating_accessor :actions, "Action"
     hash_populating_accessor :comments, "Comment"
